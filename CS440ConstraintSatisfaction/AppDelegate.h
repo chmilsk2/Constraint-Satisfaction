aba5b9e850360818fd9dcca6e7c1ec6657a5c1c3
Troy Chmieleski <chmilsk2@illinois.edu>
2013-10-21 18:51:22 -0500

    Created Xcode Project

---
+++ CS440ConstraintSatisfaction/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CS440ConstraintSatisfaction
+//
+//  Created by Troy Chmieleski on 10/21/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CS440ConstraintSatisfactionTests/CS440ConstraintSatisfactionTests.m
@@ -0,0 +1,34 @@
+//
+//  CS440ConstraintSatisfactionTests.m
+//  CS440ConstraintSatisfactionTests
+//
+//  Created by Troy Chmieleski on 10/21/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CS440ConstraintSatisfactionTests : XCTestCase
+
+@end
+
+@implementation CS440ConstraintSatisfactionTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
