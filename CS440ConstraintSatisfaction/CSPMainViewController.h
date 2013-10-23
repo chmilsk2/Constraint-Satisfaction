2e0b8c2792245956bc6d72b6464772e23354cb66
Troy Chmieleski <chmilsk2@illinois.edu>
2013-10-23 12:50:15 -0500

    conforms to list delegate protocol

---
+++ CS440ConstraintSatisfaction/CSPMainViewController.h
@@ -0,0 +1,16 @@
+//
+//  CSPMainViewController.h
+//  CS440ConstraintSatisfaction
+//
+//  Created by Troy Chmieleski on 10/21/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CSPListDelegate.h"
+
+@interface CSPMainViewController : UIViewController <CSPListDelegate>
+
+
+
+@end
