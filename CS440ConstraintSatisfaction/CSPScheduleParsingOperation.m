2e4058f2d25d38dd8ae3bb4432d96b2ba5781d8c
Troy Chmieleski <chmilsk2@illinois.edu>
2013-10-23 18:03:35 -0500

    factored out method to parse a line of space delimited numbers or "ids". Schedule parser for the schedule.

--- CS440ConstraintSatisfaction/CSPScheduleParsingOperation.m
+++ CS440ConstraintSatisfaction/CSPScheduleParsingOperation.m
@@ -78,34 +78,9 @@
 				if (assignedMeetingsLineComponents.count > 1) {
 					employeeId = [[assignedMeetingsLineComponents firstObject] integerValue];
 					
-					NSMutableArray *assignedMeetings = [NSMutableArray array];
 					NSString *assignedMeetingsStr = assignedMeetingsLineComponents[1];
 					
-					for (NSUInteger i = 0; i < assignedMeetingsStr.length; i++) {
-						NSMutableArray *meetingIdComponents = [NSMutableArray array];
-						
-						while (i < assignedMeetingsStr.length && [assignedMeetingsStr characterAtIndex:i] != ' ') {
-							char c = [assignedMeetingsStr characterAtIndex:i];
-							NSUInteger meetingIdComponent = c - '0';
-							NSNumber *meetingIdComponentNumber = [NSNumber numberWithInteger:meetingIdComponent];
-							[meetingIdComponents addObject:meetingIdComponentNumber];
-							i += 1;
-						}
-						
-						NSString *meetingIdStr = @"";
-						NSNumber *meetingIdNumber;
-						
-						for (NSUInteger i = 0; i < meetingIdComponents.count; i++) {
-							NSString *meetingIdComponentStr = [meetingIdComponents[i] stringValue];
-							meetingIdStr = [meetingIdStr stringByAppendingString:meetingIdComponentStr];
-						}
-						
-						if (meetingIdStr.length) {
-							meetingIdNumber = [NSNumber numberWithUnsignedInteger:[meetingIdStr integerValue]];
-							[assignedMeetings addObject:meetingIdNumber];
-						}
-					}
-					
+					NSArray *assignedMeetings = [self parsedIdsFromSpaceDelimitedString:assignedMeetingsStr];
 					[assignedMeetingsDict setObject:assignedMeetings forKey:[NSNumber numberWithUnsignedInteger:employeeId]];
 					
 					if (employeeId == numberOfEmployees) {
@@ -115,17 +90,77 @@
 				
 				assignedMeetingsIndex++;
 			}
+			
+			i = assignedMeetingsIndex;
 		}
 		
 		else if ([[lineComponents firstObject] isEqualToString:SCHEDULE_TRAVEL_TIME_BETWEEN_MEETINGS_KEY]) {
-			NSLog(@"%@", [NSNumber numberWithUnsignedInteger:[assignedMeetingsDict allKeys].count]);
 			NSUInteger assignedTravelTimeBetweenMeetingsIndex = i + 1;
+			
+			BOOL shouldContinue = YES;
+			
+			while (shouldContinue) {
+				NSArray *travelTimeBetweenMeetingsLineComponents = [lines[assignedTravelTimeBetweenMeetingsIndex] componentsSeparatedByString:@":"];
+				
+				NSUInteger meetingNumber;
+				
+				if (travelTimeBetweenMeetingsLineComponents.count > 1) {
+					meetingNumber = [[travelTimeBetweenMeetingsLineComponents firstObject] integerValue];
+					
+					NSString *travelTimeBetweenMeetingsStr = travelTimeBetweenMeetingsLineComponents[1];
+					
+					NSArray *travelTimeBetweenMeetingsRow = [self parsedIdsFromSpaceDelimitedString:travelTimeBetweenMeetingsStr];
+					
+					[travelTimeBetweenMeetings addObjectsFromArray:travelTimeBetweenMeetingsRow];
+					
+					if (meetingNumber == numberOfMeetings) {
+						shouldContinue = NO;
+					}
+				}
+				
+				assignedTravelTimeBetweenMeetingsIndex++;
+			}
+			
+			i = assignedTravelTimeBetweenMeetingsIndex;
 		}
 	}
 	
 	schedule = [[CSPSchedule alloc] initWithNumberOfMeetings:numberOfMeetings numberOfEmployees:numberOfEmployees numberOfTimeSlots:numberOfTimeSlots assignedMeetingsDict:assignedMeetingsDict travelTimeBetweenMeetings:travelTimeBetweenMeetings];
 	
 	[self didFinishWithSchedule:schedule Error:error];
+}
+
+#pragma mark - Parse a Line of Space Delimited Ids
+
+- (NSArray *)parsedIdsFromSpaceDelimitedString:(NSString *)string {
+	NSMutableArray *assignedIds = [NSMutableArray array];
+	
+	for (NSUInteger i = 0; i < string.length; i++) {
+		NSMutableArray *idComponents = [NSMutableArray array];
+		
+		while (i < string.length && [string characterAtIndex:i] != ' ') {
+			char c = [string characterAtIndex:i];
+			NSUInteger idComponent = c - '0';
+			NSNumber *idComponentNumber = [NSNumber numberWithInteger:idComponent];
+			[idComponents addObject:idComponentNumber];
+			i += 1;
+		}
+		
+		NSString *idStr = @"";
+		NSNumber *idNumber;
+		
+		for (NSUInteger i = 0; i < idComponents.count; i++) {
+			NSString *idComponentStr = [idComponents[i] stringValue];
+			idStr = [idStr stringByAppendingString:idComponentStr];
+		}
+		
+		if (idStr.length) {
+			idNumber = [NSNumber numberWithUnsignedInteger:[idStr integerValue]];
+			[assignedIds addObject:idNumber];
+		}
+	}
+	
+	return [assignedIds copy];
 }
 
 - (void)didFinishWithSchedule:(CSPSchedule *)schedule Error:(NSError *)error {
