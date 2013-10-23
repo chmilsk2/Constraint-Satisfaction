f28826507e984e8d5e2c10042531ab06f1779a73
Troy Chmieleski <chmilsk2@illinois.edu>
2013-10-23 12:49:06 -0500

    List Section ENUM

---
+++ CS440ConstraintSatisfaction/CSPListSection.h
@@ -0,0 +1,17 @@
+//
+//  CSPListSection.h
+//  CS440ConstraintSatisfaction
+//
+//  Created by Troy Chmieleski on 10/21/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CSPListSection : NSObject
+
+typedef NS_ENUM(NSInteger, CSPSection) {
+	CSPSectionSceduling
+};
+
+@end
