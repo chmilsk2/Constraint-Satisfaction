f5d259f4912a6dd4bb3ff096ddbaa86bd49e88d7
Troy Chmieleski <chmilsk2@illinois.edu>
2013-10-24 21:49:22 -0500

    backtracking search operation

---
+++ CS440ConstraintSatisfaction/CSPBacktrackingSearchOperation.h
@@ -0,0 +1,21 @@
+//
+//  CSPBacktrackingSearchOperation.h
+//  CS440ConstraintSatisfaction
+//
+//  Created by Troy Chmieleski on 10/23/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void(^CSPBacktrackingSearchHandler)(NSArray *assignment, NSError *);
+
+@class CSPSchedule;
+
+@interface CSPBacktrackingSearchOperation : NSOperation
+
+@property (copy) CSPBacktrackingSearchHandler backtrackingSearchCompletionBlock;
+
+- (id)initWithSchedule:(CSPSchedule *)schedule;
+
+@end
