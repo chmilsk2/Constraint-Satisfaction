060975375d9910d2fb1e2ba997869eb8096880d0
Troy Chmieleski <chmilsk2@illinois.edu>
2013-10-26 17:36:03 -0500

    added constraint satisfaction game project

---
+++ CS440ConstraintSatisfactionGame/CS440ConstraintSatisfactionGame/CSPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CSPAppDelegate.h
+//  CS440ConstraintSatisfactionGame
+//
+//  Created by Troy Chmieleski on 10/26/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CS440ConstraintSatisfactionGame/CS440ConstraintSatisfactionGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CS440ConstraintSatisfactionGame
+//
+//  Created by Troy Chmieleski on 10/26/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CSPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([CSPAppDelegate class]));
+	}
+}
