edb196202f6ae23e4947e2e14ab3e5f15fb96f6c
Troy Chmieleski <chmilsk2@illinois.edu>
2013-10-25 20:23:29 -0500

    sort meetings, unassigned meetings go at the end of the employees' meetings array

--- CS440ConstraintSatisfaction/CSPBacktrackingSearchOperation.m
+++ CS440ConstraintSatisfaction/CSPBacktrackingSearchOperation.m
@@ -13,6 +13,7 @@
 
 @implementation CSPBacktrackingSearchOperation {
 	CSPSchedule *_schedule;
+	NSMutableArray *_timeSlots;
 }
 
 - (id)initWithSchedule:(CSPSchedule *)schedule {
@@ -63,6 +64,8 @@
 		[assignment[value.unsignedIntegerValue - 1] addObject:var];
 		[var setTimeSlot:value];
 		
+		//[self prettyPrintAssignment:assignment];
+		
 		BOOL isAssignmentValid = [self isValidAssignment:assignment];
 		
 		if (isAssignmentValid) {
@@ -80,40 +83,75 @@
 	return nil;
 }
 
+- (void)prettyPrintAssignment:(NSArray *)assignment {
+	NSString *assignmentStr = @"Assignment: ";
+	
+	for (NSUInteger i = 0; i < assignment.count; i++) {
+		NSArray *timeSlotAssignment = assignment[i];
+		NSString *timeSlotAssignmentStr = @"[";
+		
+		if (timeSlotAssignment.count) {
+			for (NSUInteger j = 0; j < timeSlotAssignment.count; j++) {
+				CSPMeeting *meeting = timeSlotAssignment[j];
+				
+				if (![meeting isEqual:timeSlotAssignment.lastObject]) {
+					timeSlotAssignmentStr = [timeSlotAssignmentStr stringByAppendingString:[NSString stringWithFormat:@"%@, ", [meeting.meetingId stringValue]]];
+				}
+				
+				else {
+					timeSlotAssignmentStr = [timeSlotAssignmentStr stringByAppendingString:[NSString stringWithFormat:@"%@]", [meeting.meetingId stringValue]]];
+				}
+			}
+		}
+		
+		else {
+			timeSlotAssignmentStr = [timeSlotAssignmentStr stringByAppendingString:@"]"];
+		}
+		
+		assignmentStr = [assignmentStr stringByAppendingString:[NSString stringWithFormat:@"%lu: %@ ", (unsigned long)(i+1), timeSlotAssignmentStr]];
+	}
+	
+	NSLog(@"%@", assignmentStr);
+}
+
 #pragma mark - Order Domain Values
 
 - (NSArray *)orderDomainValuesForVar:(CSPMeeting *)var assignment:(NSArray *)assignment {
 	// in what order should the values be tried
 	// least constraining value (degree heuristic)
-	
 	// for now just return the original order of the time slots
-	NSMutableArray *timeSlots = [NSMutableArray array];
+	return self.timeSlots;
+}
+
+- (NSMutableArray *)timeSlots {
+	if (!_timeSlots) {
+		_timeSlots = [NSMutableArray array];
 	
-	for (NSUInteger i = 1; i <= _schedule.numberOfTimeSlots; i++) {
-		[timeSlots addObject:[NSNumber numberWithUnsignedInteger:i]];
+		for (NSUInteger i = 1; i <= _schedule.numberOfTimeSlots; i++) {
+			[_timeSlots addObject:[NSNumber numberWithUnsignedInteger:i]];
+		}
 	}
 	
-	return timeSlots;
+	return _timeSlots;
 }
 
 #pragma mark - Is Assignment Complete
 
 - (BOOL)isAssignmentComplete:(NSArray *)assignment {
 	BOOL isAssignmentComplete = NO;
-	BOOL isEveryAssignmentMade = YES;
+	__block BOOL isEveryAssignmentMade = YES;
 	BOOL isValidAssignment = NO;
 	
 	// check if every meeting has an assignment
-	NSArray *allKeys = _schedule.meetings.allKeys;
-	for (NSUInteger i = 0; i < allKeys.count; i++) {
-		CSPMeeting *meeting = [_schedule.meetings objectForKey:allKeys[i]];
+	[_schedule.meetings enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+		CSPMeeting *meeting = [_schedule.meetings objectForKey:key];
 		
 		if (!meeting.timeSlot) {
 			isEveryAssignmentMade = NO;
-			break;
+			*stop = YES;
 		}
-	}
-	
+	}];
+
 	if (isEveryAssignmentMade) {
 		isValidAssignment = [self isValidAssignment:assignment];
 	}
@@ -126,21 +164,23 @@
 }
 
 - (BOOL)isValidAssignment:(NSArray *)assignment {
-	BOOL isValidAssignment = NO;
-	
 	// assignment is complete if all the constraints are satisfied
 	
 	// check that no employee has more than 1 meeting during a particular time slot
 	BOOL hasNoMoreThanOneMeetingPerTimeSlot = [self hasNoMoreThanOneMeetingPerTimeSlotForAssignment:assignment];
 	
+	if (!hasNoMoreThanOneMeetingPerTimeSlot) {
+		return NO;
+	}
+	
 	// check that the employee can make it to all of the assigned meetings
 	BOOL hasSatisfiableTravelTimes = [self hasSatisfiableTravelTimesForAssignment:assignment];
 	
-	if (hasNoMoreThanOneMeetingPerTimeSlot && hasSatisfiableTravelTimes) {
-		isValidAssignment = YES;
+	if (!hasSatisfiableTravelTimes) {
+		return NO;
 	}
 	
-	return isValidAssignment;
+	return YES;
 }
 
 - (BOOL)hasNoMoreThanOneMeetingPerTimeSlotForAssignment:(NSArray *)assignment {
@@ -153,11 +193,27 @@
 			
 			for (CSPEmployee *employee in employees) {
 				// make sure no time slotted meetings are at the same time for a given employee
-				for (NSUInteger i = 0; i < employee.meetings.count; i++) {
-					for (NSUInteger j = i+1; j < employee.meetings.count; j++) {
-						if ([employee.meetings[i] isEqual:employee.meetings[j]]) {
-							return NO;
+				for (NSUInteger i = 0; i < employee.meetings.count; i++) {					
+					CSPMeeting *meeting1 = employee.meetings[i];
+				
+					if (meeting1.timeSlot) {
+						for (NSUInteger j = i+1; j < employee.meetings.count; j++) {
+							CSPMeeting *meeting2 = employee.meetings[j];
+							
+							if (meeting2.timeSlot) {
+								if (meeting1.timeSlot.unsignedIntegerValue == meeting2.timeSlot.unsignedIntegerValue) {
+									return NO;
+								}
+							}
+							
+							else {
+								break;
+							}
 						}
+					}
+					
+					else {
+						break;
 					}
 				}
 			}
@@ -168,47 +224,38 @@
 }
 
 - (BOOL)hasSatisfiableTravelTimesForAssignment:(NSArray *)assignment {
-	NSMutableDictionary *employeesChecked = [NSMutableDictionary dictionary];
-	
 	// loop through all of the time slots in the assignment
-	for (NSUInteger timeSlot = 0; timeSlot < _schedule.numberOfTimeSlots; timeSlot++) {
+	for (NSUInteger timeSlot = 1; timeSlot <= _schedule.numberOfTimeSlots; timeSlot++) {
 		// loop through all of the meetings in the assignment
-		NSArray *meetings = assignment[timeSlot];
+		NSArray *meetings = assignment[timeSlot - 1];
 		
 		for (CSPMeeting *meeting in meetings) {
 			// loop through all of the employees associated with the meeting
 			NSArray *employees = meeting.employees;
 			
 			for (CSPEmployee *employee in employees) {
-				if (![employeesChecked objectForKey:employee.employeeId]) {
-					// sort the meetings for the employee
-					NSMutableArray *timeSlottedMeetings = [NSMutableArray array];
+				// meetings without time slots are placed at the end of the array
+				[self sortMeetingsByTimeSlot:employee.meetings];
+			
+				// make sure the employee can make the next meeting
+				for (NSUInteger i = 0; i < employee.meetings.count - 1; i++) {
+					CSPMeeting *meeting1 = employee.meetings[i];
 					
-					for (CSPMeeting *meeting in employee.meetings) {
-						if (meeting.timeSlot) {
-							[timeSlottedMeetings addObject:meeting];
+					if (meeting1.timeSlot) {
+						if (meeting1.timeSlot.unsignedIntegerValue == timeSlot) {
+							CSPMeeting *meeting2 = employee.meetings[i+1];
+							
+							NSUInteger delta = meeting2.timeSlot.unsignedIntegerValue - meeting1.timeSlot.unsignedIntegerValue;
+							
+							if (delta <= [_schedule travelTimeBetweenMeeting1:meeting1 meeting2:meeting2]) {
+								return NO;
+							}
 						}
 					}
 					
-					NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"timeSlot"
-																				   ascending:YES];
-					NSArray *sortDescriptors = [NSArray arrayWithObject:sortDescriptor];
-					NSArray *sortedArray = [timeSlottedMeetings sortedArrayUsingDescriptors:sortDescriptors];
-					
-					// make sure the employee can make the next meeting
-					for (NSUInteger i = 0; i < sortedArray.count - 1; i++) {
-						CSPMeeting *meeting1 = sortedArray[i];
-						CSPMeeting *meeting2 = sortedArray[i+1];
-						
-						NSUInteger delta = meeting2.timeSlot.unsignedIntegerValue - meeting1.timeSlot.unsignedIntegerValue;
-						
-						if (delta <= [_schedule travelTimeBetweenMeeting1:meeting1 meeting2:meeting2]) {
-							return NO;
-						}
+					else {
+						break;
 					}
-					
-					// if the employees travel times have already been checked for consistency, do not check them again
-					[employeesChecked setObject:employee forKey:employee.employeeId];
 				}
 			}
 			
@@ -222,20 +269,49 @@
 
 - (CSPMeeting *)selectUnassignedVariable {
 	// the variables for the CSP are the meetings
-	CSPMeeting *unassignedMeeting;
+	__block CSPMeeting *unassignedMeeting;
 	
 	// for now, just select a random variable that is not assigned
-	NSArray *allKeys = _schedule.meetings.allKeys;
-	for (NSUInteger i = 0; i < allKeys.count; i++) {
-		CSPMeeting *meeting = [_schedule.meetings objectForKey:allKeys[i]];
+	[_schedule.meetings enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+		CSPMeeting *meeting = [_schedule.meetings objectForKey:key];
 		
 		if (!meeting.timeSlot) {
 			unassignedMeeting = meeting;
-			break;
+			*stop = YES;
 		}
-	}
+	}];
 	
 	return unassignedMeeting;
+}
+
+#pragma mark - Sort Meetings
+
+- (void)sortMeetingsByTimeSlot:(NSMutableArray *)meetings {
+	[meetings sortUsingComparator:^NSComparisonResult(CSPMeeting *meeting1, CSPMeeting *meeting2) {
+		if (!meeting1.timeSlot || !meeting2.timeSlot) {
+			if (!meeting1.timeSlot && !meeting2.timeSlot) {
+				return (NSComparisonResult)NSOrderedSame;
+			}
+			
+			if (!meeting1.timeSlot) {
+				return (NSComparisonResult)NSOrderedDescending;
+			}
+			
+			if (!meeting2.timeSlot) {
+				return (NSComparisonResult)NSOrderedAscending;
+			}
+		}
+		
+		if (meeting1.timeSlot.unsignedIntegerValue > meeting2.timeSlot.unsignedIntegerValue) {
+			return (NSComparisonResult)NSOrderedDescending;
+		}
+		
+		if (meeting1.timeSlot.unsignedIntegerValue < meeting2.timeSlot.unsignedIntegerValue) {
+			return (NSComparisonResult)NSOrderedAscending;
+		}
+		
+		return (NSComparisonResult)NSOrderedSame;
+	}];
 }
 
 - (void)didFinishWithAssignment:(NSArray *)assignment error:(NSError *)error {
