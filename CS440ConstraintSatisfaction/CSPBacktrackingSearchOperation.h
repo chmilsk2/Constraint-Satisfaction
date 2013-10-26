43c903978446e427a3ef32fed25d14f218a298be
Troy Chmieleski <chmilsk2@illinois.edu>
2013-10-26 16:01:23 -0500

    callback now returns a time interval to measure performance up to millisecond accuracy

--- CS440ConstraintSatisfaction/CSPBacktrackingSearchOperation.h
+++ CS440ConstraintSatisfaction/CSPBacktrackingSearchOperation.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-typedef void(^CSPBacktrackingSearchHandler)(NSArray *assignment, NSError *);
+typedef void(^CSPBacktrackingSearchHandler)(NSArray *assignment, NSUInteger, NSTimeInterval, NSError *);
 
 @class CSPSchedule;
 
