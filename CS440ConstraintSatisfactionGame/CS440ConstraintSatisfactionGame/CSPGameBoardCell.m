c76ce7a4267c920c225c7ada68e43fd3833832c6
Troy Chmieleski <chmilsk2@illinois.edu>
2013-10-27 19:10:11 -0500

    game board cell

---
+++ CS440ConstraintSatisfactionGame/CS440ConstraintSatisfactionGame/CSPGameBoardCell.m
@@ -0,0 +1,24 @@
+//
+//  CSPGameBoardCell.m
+//  CS440ConstraintSatisfactionGame
+//
+//  Created by Troy Chmieleski on 10/27/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import "CSPGameBoardCell.h"
+
+@implementation CSPGameBoardCell
+
+- (id)initWithWeight:(NSUInteger)weight {
+	self = [super init];
+	
+	if (self) {
+		_weight = weight;
+		_owner = CSPCellOwnerNone;
+	}
+	
+	return self;
+}
+
+@end
