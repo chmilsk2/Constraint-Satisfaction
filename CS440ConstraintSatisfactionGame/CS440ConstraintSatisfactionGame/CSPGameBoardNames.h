07958377981dfec204ff6f70ee8f51c504202ed8
Troy Chmieleski <chmilsk2@illinois.edu>
2013-10-27 19:11:16 -0500

    game board names

---
+++ CS440ConstraintSatisfactionGame/CS440ConstraintSatisfactionGame/CSPGameBoardNames.h
@@ -0,0 +1,15 @@
+//
+//  CSPGameBoardNames.h
+//  CS440ConstraintSatisfactionGame
+//
+//  Created by Troy Chmieleski on 10/26/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define GAME_BOARD_NAME_KEREN @"Keren"
+#define GAME_BOARD_NAME_NARVIK @"Narvik"
+#define GAME_BOARD_NAME_SEVASTOPOL @"Sevastopol"
+#define GAME_BOARD_NAME_SMOLENSK @"Smolensk"
+#define GAME_BOARD_NAME_WESTERPLATTE @"Westerplatte"
