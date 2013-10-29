c0bbe047b0d1338a657e42001dbb0ae8de967c69
Troy Chmieleski <chmilsk2@illinois.edu>
2013-10-29 15:34:28 -0500

    algorithm operation to works for minimax

--- CS440ConstraintSatisfactionGame/CS440ConstraintSatisfactionGame/CSPAlgorithmOperation.h
+++ CS440ConstraintSatisfactionGame/CS440ConstraintSatisfactionGame/CSPAlgorithmOperation.h
@@ -7,13 +7,24 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "CSPGameBoard.h"
+#import "CSPNode.h"
+
+@class CSPNode;
 
 typedef void(^CSPAlgorithmOperationHandler)(NSUInteger row, NSUInteger col, NSError *);
 
 @interface CSPAlgorithmOperation : NSOperation
 
 @property (copy)CSPAlgorithmOperationHandler aglorithmOperationCompletionBlock;
+@property CSPNode *root;
 
+- (id)initWithRoot:(CSPNode *)root;
+- (BOOL)isTerminalNode:(CSPNode *)node;
+- (NSArray *)childrenForNode:(CSPNode *)node;
+- (NSInteger)heuristicValueOfNode:(CSPNode *)node;
+- (CSPNode *)findOptimalNode;
+- (CGPoint)optimalLocationForNode:(CSPNode *)node;
 - (void)didFinishWithRow:(NSUInteger)row col:(NSUInteger)col error:(NSError *)error;
 
 @end
