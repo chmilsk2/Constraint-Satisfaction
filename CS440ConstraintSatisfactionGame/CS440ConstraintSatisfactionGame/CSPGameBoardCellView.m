a9068455c85bff54914ddb710d88cbe77df0ca7a
Troy Chmieleski <chmilsk2@illinois.edu>
2013-10-28 20:01:44 -0500

    change background color of cell when a move is made and the opponent conquers the piece

--- CS440ConstraintSatisfactionGame/CS440ConstraintSatisfactionGame/CSPGameBoardCellView.m
+++ CS440ConstraintSatisfactionGame/CS440ConstraintSatisfactionGame/CSPGameBoardCellView.m
@@ -18,6 +18,10 @@
 	NSUInteger _owner;
 	UIColor *_backgroundColor;
 	UIColor *_borderColor;
+	UIColor *_player1Color;
+	UIColor *_player2Color;
+	UIColor *_playerTextColor;
+	UIColor *_noneTextColor;
 	UIFont *_font;
 }
 
@@ -30,6 +34,13 @@
 		
 		_backgroundColor = backgroundColor;
 		_borderColor = borderColor;
+		
+		_player1Color = [UIColor colorWithRed:253.0/255.0 green:134.0/255.0 blue:9.0/255.0 alpha:1.0];
+		_player2Color = [UIColor colorWithRed:100.0/255.0 green:100.0/255.0 blue:100.0/255.0 alpha:1.0];
+		
+		_playerTextColor = [UIColor whiteColor];
+		_noneTextColor = [UIColor blackColor];
+		
 		_font = [UIFont fontWithName:GAME_BOARD_CELL_FONT_NAME size:GAME_BOARD_CELL_FONT_SIZE];
 		
 		// set background color
@@ -37,6 +48,28 @@
     }
 	
     return self;
+}
+
+#pragma mark - Respond to Touch Events
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+	if ([self.delegate respondsToSelector:@selector(touchedCellFrame:)]) {
+		[self.delegate touchedCellFrame:self.frame];
+	}
+}
+
+- (void)updateWithOwner:(CSPOwner)owner {
+	_owner = owner;
+	
+	if (_owner == CSPOwnerPlayer1) {
+		[self setBackgroundColor:_player1Color];
+	}
+	
+	else {
+		[self setBackgroundColor:_player2Color];
+	}
+	
+	[self setNeedsDisplay];
 }
 
 - (void)drawRect:(CGRect)rect {
@@ -58,7 +91,17 @@
     textStyle.lineBreakMode = NSLineBreakByClipping;
     textStyle.alignment = NSTextAlignmentCenter;
 	
-	NSDictionary *attributes = @{NSFontAttributeName:_font, NSParagraphStyleAttributeName:textStyle};
+	UIColor *textColor;
+
+	if (_owner == CSPOwnerNone) {
+		textColor = _noneTextColor;
+	}
+	
+	else {
+		textColor = _playerTextColor;
+	}
+	
+	NSDictionary *attributes = @{NSFontAttributeName:_font, NSParagraphStyleAttributeName:textStyle, NSForegroundColorAttributeName:textColor};
 	
 	[_weight drawInRect:textRect withAttributes:attributes];
 }
