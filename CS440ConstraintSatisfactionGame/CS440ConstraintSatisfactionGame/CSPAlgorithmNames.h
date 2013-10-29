1615bc4c87085290230149ef3a9eb02e6aff7587
Troy Chmieleski <chmilsk2@illinois.edu>
2013-10-28 19:56:45 -0500

    algorithm names

---
+++ CS440ConstraintSatisfactionGame/CS440ConstraintSatisfactionGame/CSPAlgorithmNames.h
@@ -0,0 +1,13 @@
+//
+//  CSPAlgorithmNames.h
+//  CS440ConstraintSatisfactionGame
+//
+//  Created by Troy Chmieleski on 10/28/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define ALGORITHM_MINIMAX_NAME @"Minimax"
+#define ALGORITHM_ALPHA_BETA_NAME @"Alpha Beta"
+
