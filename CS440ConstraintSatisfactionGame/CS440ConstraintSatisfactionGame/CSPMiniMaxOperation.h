a0256abb18f32f20a913e424628d3277064fd2ee
Troy Chmieleski <chmilsk2@illinois.edu>
2013-10-28 20:03:15 -0500

    minimax operation

---
+++ CS440ConstraintSatisfactionGame/CS440ConstraintSatisfactionGame/CSPMiniMaxOperation.h
@@ -0,0 +1,13 @@
+//
+//  CSPMiniMaxOperation.h
+//  CS440ConstraintSatisfactionGame
+//
+//  Created by Troy Chmieleski on 10/26/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import "CSPAlgorithmOperation.h"
+
+@interface CSPMiniMaxOperation : CSPAlgorithmOperation
+
+@end
---
+++ CS440ConstraintSatisfactionGame/CS440ConstraintSatisfactionGame/CSPMiniMaxOperation.m
@@ -0,0 +1,13 @@
+//
+//  CSPMiniMaxOperation.m
+//  CS440ConstraintSatisfactionGame
+//
+//  Created by Troy Chmieleski on 10/26/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import "CSPMiniMaxOperation.h"
+
+@implementation CSPMiniMaxOperation
+
+@end
