78b739d1d124d1adb782720cadc16697ac5d7562
Troy Chmieleski <chmilsk2@illinois.edu>
2013-10-28 19:57:50 -0500

    alpha beta operation

---
+++ CS440ConstraintSatisfactionGame/CS440ConstraintSatisfactionGame/CSPAlphaBetaOperation.m
@@ -0,0 +1,13 @@
+//
+//  CSPAlphaBetaOperation.m
+//  CS440ConstraintSatisfactionGame
+//
+//  Created by Troy Chmieleski on 10/26/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import "CSPAlphaBetaOperation.h"
+
+@implementation CSPAlphaBetaOperation
+
+@end
