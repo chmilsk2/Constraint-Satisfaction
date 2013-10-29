715689535516f3a9514157fb059f6514f7220f4d
Troy Chmieleski <chmilsk2@illinois.edu>
2013-10-28 19:59:03 -0500

    cell owner

---
+++ CS440ConstraintSatisfactionGame/CS440ConstraintSatisfactionGame/CSPCellOwner.h
@@ -0,0 +1,15 @@
+//
+//  CSPCellOwner.h
+//  CS440ConstraintSatisfactionGame
+//
+//  Created by Troy Chmieleski on 10/27/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSUInteger, CSPOwner) {
+	CSPOwnerNone,
+	CSPOwnerPlayer1,
+	CSPOwnerPlayer2
+};
